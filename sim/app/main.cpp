136cfb7f758b2dbb74f253e6ac788dbc8047f309
Frank Poole <fpoolev@gmail.com>
2015-11-09 17:55:04 -0800

    Added missing sim files.

---
+++ sim/app/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "foo.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, Sense and Avoid Surveillance Simulator!" << endl;
+
+    foo foolish;
+    return foolish.getZero();
+}
