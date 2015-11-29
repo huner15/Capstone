8ef2a8aa7cd6f0b52217e585b2079546b9a5a2f9
huner15 <mchimp@hotmail.com>
2015-11-29 11:01:51 -0800

    unity inital commit/networking code
    probably working network code need images still for display

---
+++ cdti/CDTIUnity/Assets/scripts/NetworkInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+using System;
+
+public class NetworkInput : MonoBehaviour {
+
+    TcpListener server = null;
+    NetworkStream stream;
+	// Use this for initialization
+	void Start () {
+        Int32 port = 13000;
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+        // TcpListener server = new TcpListener(port);
+        server = new TcpListener(localAddr, port);
+
+        // Start listening for client requests.
+        server.Start();
+
+        Console.WriteLine("waiting fr a connection... ");
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Connected");
+
+        stream = client.GetStream();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //try to read in the client info here.
+
+	}
+}
