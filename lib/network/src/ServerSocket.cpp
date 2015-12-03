e3fb5fb33c8e90387a6cea6e4284fb045c3d4652
Frank Poole <fpoolev@gmail.com>
2015-12-02 16:33:00 -0800

    Fixed to_string include error in ServerSocket.cpp.

--- lib/network/src/ServerSocket.cpp
+++ lib/network/src/ServerSocket.cpp
@@ -1,28 +1,16 @@
+#include <string>
+#include <iostream>
+#include <sstream>
 #include <arpa/inet.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/descriptor.h>
-#include <iostream>
-#include <cstring>
-#include "ProtobufSocketSerializer.h"
 
+#include "ProtobufSocketSerializer.h"
 #include "ServerSocket.h"
 #include "SocketException.h"
 
-#include <string>
-#include <sstream>
-
 #define PROTOBUF_HEADER_LEN 4
-
-//TODO: GET RID OF THIS
-template <typename T>
-std::string to_string(T value)
-{
-   std::ostringstream os ;
-   os << value ;
-   return os.str() ;
-}
-
 
 ServerSocket::ServerSocket(int port)
 {
@@ -33,7 +21,7 @@ ServerSocket::ServerSocket(int port)
    }
    if(! Socket::bind(port))
    {
-      throw SocketException("Could not bind to port" + to_string(port));
+      throw SocketException("Could not bind to port" + std::to_string(port));
    }
    if(! Socket::listen())
    {
