c72ba9257d1b2a63113788192bed7c8f41b6bc82
Frank Poole <fpoolev@gmail.com>
2016-01-16 17:47:17 -0800

    Refactored the network library. Cleaned up CLion warnings, code style, simplified some logic, added comments.

--- lib/network/src/ClientSocket.cpp
+++ lib/network/src/ClientSocket.cpp
@@ -1,35 +1,31 @@
+/*
+ * California Polytechnic State University, San Luis Obispo
+ * Computer Engineering - CPE 402, 405, 406
+ * Author: Frank Poole
+ * Professor: David Janzen
+ * Date: 1-16-2016
+ * Disclaimer: The original version of the following code was taken from
+ * tldp.org/LDP/LG/issue74/misc/tougher
+ */
+
 #include "ClientSocket.h"
-#include "SocketException.h"
-#include "ProtobufSocketSerializer.h"
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <google/protobuf/io/zero_copy_stream_impl.h>
-#include <google/protobuf/io/coded_stream.h>
-#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
-#include <google/protobuf/descriptor.h>
-#include <iostream>
-#include <string>
-#include <sstream>
-
-#define PROTOBUF_HEADER_LEN 4
-
-//TODO: GET RID OF THIS
-using namespace std;
 
 template<typename T>
 std::string to_string(T value) {
     std::ostringstream os;
     os << value;
+
     return os.str();
 }
 
-ClientSocket::ClientSocket(std::string host, int port) {
+ClientSocket::ClientSocket(std::string host, const in_port_t port) {
     if (!Socket::create()) {
         throw SocketException("Could not create client socket.");
     }
+
     if (!Socket::connect(host, port)) {
-        throw SocketException("Could not connet to" + host + ":" + to_string(port));
+        throw SocketException("Could not connect to" + host + ":" +
+                              to_string(port) + ".");
     }
 }
 
@@ -49,62 +45,79 @@ const ClientSocket &ClientSocket::operator>>(std::string &s) const {
     return *this;
 }
 
-const ClientSocket &ClientSocket::operator<<(const ::google::protobuf::Message &msg) const {
+const ClientSocket &ClientSocket::operator<<(
+        const ::google::protobuf::Message &msg) const {
     char *packet;
-    int len = ProtobufSocketSerializer::serialize(msg, packet);
+    size_t len = ProtobufSocketSerializer::serialize(msg, packet);
+
     if (!Socket::send(packet, len)) {
         throw SocketException("Could not write to socket.");
     }
+
     delete packet;
+
     return *this;
 }
 
-//Taken from http://stackoverflow.com/questions/9496101/protocol-buffer-over-socket-in-c
-::google::protobuf::uint32 ClientSocket::read_protobuf_header(const char *header) const {
+::google::protobuf::uint32 ClientSocket::read_protobuf_header(
+        const char *header) const {
     ::google::protobuf::uint32 size;
-    char buf[PROTOBUF_HEADER_LEN];
+    char buf[PROTOBUF_HEADER_LENGTH];
 
-    memcpy(buf, header, PROTOBUF_HEADER_LEN);
-    ::google::protobuf::io::ArrayInputStream ais(buf, PROTOBUF_HEADER_LEN);
+    memcpy(buf, header, PROTOBUF_HEADER_LENGTH);
+    ::google::protobuf::io::ArrayInputStream ais(buf, PROTOBUF_HEADER_LENGTH);
     ::google::protobuf::io::CodedInputStream coded_input(&ais);
     coded_input.ReadVarint32(&size);//Decode the header and get the size
 
     return size;
 }
 
-void ClientSocket::read_protobuf(::google::protobuf::Message &msg, const char *data) const {
-    //Read the header again to determine the message length (Could be refactored?)
+void ClientSocket::read_protobuf(::google::protobuf::Message &msg,
+                                 const char *data) const {
+    // Read the header again to determine the message length.
     ::google::protobuf::uint32 size = read_protobuf_header(data);
-    char buf[PROTOBUF_HEADER_LEN + size];
-    memcpy(buf, data, PROTOBUF_HEADER_LEN + size);
+    char buf[PROTOBUF_HEADER_LENGTH + size];
+    memcpy(buf, data, PROTOBUF_HEADER_LENGTH + size);
 
-    ::google::protobuf::io::ArrayInputStream ais(buf, PROTOBUF_HEADER_LEN + size);
-    //Re-read the input length
+    ::google::protobuf::io::ArrayInputStream ais(
+            buf, PROTOBUF_HEADER_LENGTH + size);
+
+    // Re-read the input length
     ::google::protobuf::io::CodedInputStream coded_input(&ais);
-    //Read an unsigned integer with Varint encoding, truncating to 32 bits.
+
+    // Read an unsigned integer with variant encoding, truncating to 32 bits.
     ::google::protobuf::uint32 siz;
     coded_input.ReadVarint32(&siz);
-    //read the remainder of the message
-    ::google::protobuf::io::CodedInputStream::Limit msgLimit = coded_input.PushLimit(siz);
+
+    // Read the remainder of the message
+    ::google::protobuf::io::CodedInputStream::Limit msgLimit =
+            coded_input.PushLimit(siz);
+
     //De-Serialize
     msg.ParseFromCodedStream(&coded_input);
-    //Once the embedded message has been parsed, PopLimit() is called to undo the limit
+
+    // Once the embedded message has been parsed, PopLimit() is called to
+    // undo the limit
     coded_input.PopLimit(msgLimit);
 }
 
-const ClientSocket &ClientSocket::operator>>(::google::protobuf::Message &msg) const {
-    //Read the first four bytes
-    char lenBfr[PROTOBUF_HEADER_LEN];
-    if (!Socket::recv(&lenBfr, PROTOBUF_HEADER_LEN, MSG_PEEK)) {
-        throw SocketException("Could not read header from socket");
+const ClientSocket &ClientSocket::operator>>(
+        ::google::protobuf::Message &msg) const {
+    char lenBfr[PROTOBUF_HEADER_LENGTH];
+
+    // Read the protocol buffer size header (first four bytes).
+    if (!Socket::recv(&lenBfr, PROTOBUF_HEADER_LENGTH, MSG_PEEK)) {
+        throw SocketException("Could not read header from socket.");
     }
+
+    // Read the protocol buffer data.
     ::google::protobuf::uint32 len = read_protobuf_header(lenBfr);
-    char dataBuf[len + PROTOBUF_HEADER_LEN];
-    if (!Socket::recv(dataBuf, len + PROTOBUF_HEADER_LEN, MSG_WAITALL)) {
-        throw SocketException("Could not read from socket");
+    char dataBuf[PROTOBUF_HEADER_LENGTH + len];
+    if (!Socket::recv(dataBuf, len + PROTOBUF_HEADER_LENGTH, MSG_WAITALL)) {
+        throw SocketException("Could not read from socket.");
     }
+
     read_protobuf(msg, dataBuf);
-    //Parse data and assign to msg
-    //
+
     return *this;
 }
--- lib/network/src/ProtobufSocketSerializer.cpp
+++ lib/network/src/ProtobufSocketSerializer.cpp
@@ -1,18 +1,21 @@
-#include <iostream>
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/io/zero_copy_stream_impl.h>
-#include <google/protobuf/io/coded_stream.h>
+/*
+ * California Polytechnic State University, San Luis Obispo
+ * Computer Engineering - CPE 402, 405, 406
+ * Author: Frank Poole
+ * Professor: David Janzen
+ * Date: 1-16-2016
+ * Disclaimer: The original version of the following code was taken from
+ * tldp.org/LDP/LG/issue74/misc/tougher
+ */
 
 #include "ProtobufSocketSerializer.h"
 
-#define HEADER_LEN 4
-
-int ProtobufSocketSerializer::serialize(
+size_t ProtobufSocketSerializer::serialize(
         const ::google::protobuf::Message &msg, char *&packet) {
-    int size = msg.ByteSize() + HEADER_LEN;
+    size_t size = PROTOBUF_HEADER_LENGTH + (size_t) msg.ByteSize();
     packet = new char[size]();
 
-    ::google::protobuf::io::ArrayOutputStream aos(packet, size);
+    ::google::protobuf::io::ArrayOutputStream aos(packet, (int) size);
     ::google::protobuf::io::CodedOutputStream coded_output(&aos);
     coded_output.WriteVarint32((google::protobuf::uint32) msg.ByteSize());
     msg.SerializeToCodedStream(&coded_output);
