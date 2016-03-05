0e4fd4345b26353deb2c1638c93447cca0ce5eac
Andrea Savage <andrea@davidsavage.org>
2016-03-05 09:42:11 -0800

    Updated distance tests.

--- saap/algorithm/test/DistanceTest.cpp
+++ saap/algorithm/test/DistanceTest.cpp
@@ -9,30 +9,150 @@
 #include <gtest/gtest.h>
 #include "CorrelationEngine.h"
 
-TEST(CalcHeading, tcasAndAdsb) {
+TEST(TcasAdsb, outofrange) {
+    //setup data
     std::time_t time = 1;
     TailNumber tailNumber = TailNumber();
     TcasID tcasID = TcasID();
     RadarID radarID = RadarID();
-    GeographicCoordinate geographicCoordinate = GeographicCoordinate(10, 20,
-                                                                     30);
-    SphericalCoordinate sphericalCoordinate = SphericalCoordinate(15, 25, 35);
-    Velocity velocity = Velocity(2, 3, 4);
+    Velocity velocity = Velocity();
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate();
+    CorrelationEngine engine = CorrelationEngine();
+
+    //test data
+    SphericalCoordinate sphericalCoordinate = SphericalCoordinate(5, 10, 20);
     Device device = ADSB;
+    Velocity velocity = Velocity(1, 2, 3);
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate(10, 10,
+                                                                     20);
+    SurveillanceReport reportOne = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
 
-    FlightReport flightReport = FlightReport(time, tailNumber, tcasID,
-                                             radarID, geographicCoordinate,
-                                            sphericalCoordinate, velocity,
-                                             device);
+    SphericalCoordinate(100, 100, 100);
+    device = TCAS;
+    Velocity velocity = Velocity(100, 100, 100);
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate(100, 100, 100);
+    SurveillanceReport reportTwo = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
 
-    AdsBReport adsBReport = flightReport.CreateAdsBReport();
+    EXPECT_EQ(0, engine.CalcHeading(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcVelocity(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcEuclidDistance(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcDistance(&reportOne, &reportTwo));
+}
 
-    EXPECT_EQ(time, adsBReport.timestamp());
-    EXPECT_EQ(geographicCoordinate.GetLatitude(), adsBReport.latitude());
-    EXPECT_EQ(geographicCoordinate.GetLongitude(), adsBReport.longitude());
-    EXPECT_EQ(geographicCoordinate.GetAltitude(), adsBReport.altitude());
-    EXPECT_EQ(tailNumber.Get(), adsBReport.tail_number());
-    EXPECT_EQ(velocity.north, adsBReport.north());
-    EXPECT_EQ(velocity.down, adsBReport.down());
-    EXPECT_EQ(velocity.east, adsBReport.east());
+TEST(radarAdsb, outofrange) {
+    //setup data
+    std::time_t time = 1;
+    TailNumber tailNumber = TailNumber();
+    TcasID tcasID = TcasID();
+    RadarID radarID = RadarID();
+    Velocity velocity = Velocity();
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate();
+    CorrelationEngine engine = CorrelationEngine();
+
+    //test data
+    SphericalCoordinate sphericalCoordinate = SphericalCoordinate(5, 10, 20);
+    Device device = ADSB;
+    Velocity velocity = Velocity(1, 2, 3);
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate(10, 10,
+                                                                     20);
+    SurveillanceReport reportOne = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
+
+    SphericalCoordinate(100, 100, 100);
+    device = RADAR;
+    Velocity velocity = Velocity(100, 100, 100);
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate(100, 100, 100);
+    SurveillanceReport reportTwo = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
+
+    EXPECT_EQ(0, engine.CalcHeading(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcVelocity(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcEuclidDistance(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcDistance(&reportOne, &reportTwo));
+}
+
+TEST(radarTcas, outofrange) {
+    //setup data
+    std::time_t time = 1;
+    TailNumber tailNumber = TailNumber();
+    TcasID tcasID = TcasID();
+    RadarID radarID = RadarID();
+    CorrelationEngine engine = CorrelationEngine();
+
+    //test data
+    SphericalCoordinate sphericalCoordinate = SphericalCoordinate(5, 10, 20);
+    Device device = ADSB;
+    Velocity velocity = Velocity(1, 2, 3);
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate(10, 10,
+                                                                     20);
+    SurveillanceReport reportOne = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
+
+    SphericalCoordinate(100, 100, 100);
+    device = RADAR;
+    Velocity velocity = Velocity(100, 100, 100);
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate(100, 100, 100);
+    SurveillanceReport reportTwo = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
+
+    EXPECT_EQ(0, engine.CalcHeading(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcVelocity(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcEuclidDistance(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcDistance(&reportOne, &reportTwo));
+}
+
+TEST(TcasAdsb, inrange) {
+    //setup data
+    std::time_t time = 1;
+    TailNumber tailNumber = TailNumber();
+    TcasID tcasID = TcasID();
+    RadarID radarID = RadarID();
+    Velocity velocity = Velocity();
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate();
+    CorrelationEngine engine = CorrelationEngine();
+
+    //test data
+    SphericalCoordinate sphericalCoordinate = SphericalCoordinate(5, 10, 20);
+    Device device = ADSB;
+    SurveillanceReport reportOne = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
+
+    SphericalCoordinate(100, 100, 100);
+    device = TCAS;
+    SurveillanceReport reportTwo = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
+
+    EXPECT_EQ(0, engine.CalcHeading(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcVelocity(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcEuclidDistance(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcDistance(&reportOne, &reportTwo));
+}
+
+TEST(TcasAdsb, onrange) {
+    //setup data
+    std::time_t time = 1;
+    TailNumber tailNumber = TailNumber();
+    TcasID tcasID = TcasID();
+    RadarID radarID = RadarID();
+    Velocity velocity = Velocity();
+    GeographicCoordinate geographicCoordinate = GeographicCoordinate();
+    CorrelationEngine engine = CorrelationEngine();
+
+    //test data
+    SphericalCoordinate sphericalCoordinate = SphericalCoordinate(5, 10, 20);
+    Device device = ADSB;
+    SurveillanceReport reportOne = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
+
+    SphericalCoordinate(100, 100, 100);
+    device = TCAS;
+    SurveillanceReport reportTwo = SurveillanceReport(time, tailNumber, tcasID,
+                                                      radarID, geographicCoordinate, sphericalCoordinate, velocity, device);
+
+    EXPECT_EQ(0, engine.CalcHeading(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcVelocity(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcEuclidDistance(&reportOne, &reportTwo));
+    EXPECT_EQ(0, engine.CalcDistance(&reportOne, &reportTwo));
 }
