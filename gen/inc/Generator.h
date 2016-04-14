d682fc8c3684316252dfed3833abd7dbb66aaea0
fpoolev <fpoolev@gmail.com>
2016-04-13 19:03:37 -0700

    Fixed SA-101 run_gen executable fails on incorrect file names.

--- gen/inc/Generator.h
+++ gen/inc/Generator.h
@@ -1,14 +1,18 @@
-//
-// Created by kevin on 3/9/16.
-//
+/*
+ * @file Generator.h
+ * @author Specific Atomics
+ * @authors Kevin Pham, Frank Poole
+ * @date 4-13-16
+ * @brief TODO: Description
+ */
 
-#ifndef SAAS_GENERATOR_H
-#define SAAS_GENERATOR_H
+#ifndef SAAS_GENERATOR_H_
+#define SAAS_GENERATOR_H_
 
 #include "SimulationGenerator.h"
 #include "SaasUtil.h"
 
 /** Expected number of command line arguments. */
-const int EXPECTED_ARGUMENTS = 2;
+const int EXPECTED_ARGUMENTS = 3;
 
-#endif //SAAS_GENERATOR_H
+#endif
--- gen/src/Generator.cpp
+++ gen/src/Generator.cpp
@@ -1,19 +1,31 @@
-//
-// Created by kevin on 3/9/16.
-//
+/*
+ * @file Generator.cpp
+ * @author Specific Atomics
+ * @authors Kevin Pham, Frank Poole
+ * @date 4-13-16
+ * @brief TODO: Description
+ */
 
 #include "Generator.h"
+
 int main(int argc, char *argv[]) {
 
-    if (argc != EXPECTED_ARGUMENTS) {
-        std::cout << "usage: run_gen file_name" << std::endl;
+    if (argc == EXPECTED_ARGUMENTS) {
+        try {
+            SimulationGenerator gen(argv[1]);
+
+            std::ofstream file_id;
+            file_id.open(argv[2], std::fstream::out);
+            Json::StyledWriter styled_writer;
+            file_id << styled_writer.write(gen.WriteReports());
+            file_id.close();
+        }
+        catch (std::invalid_argument exception) {
+            std::cout << "Invalid generation file." << std::endl;
+        }
     }
     else {
-        std::ofstream file_id;
-        SimulationGenerator gen(argv[1]);
-        file_id.open("FinalSim.json", std::fstream::out);
-        Json::StyledWriter styled_writer;
-        file_id << styled_writer.write(gen.WriteReports());
-        file_id.close();
+        std::cout << "usage: run_gen input_file_name output_file_name"
+        << std::endl;
     }
 }
